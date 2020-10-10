c8df691d0795b9f90ba638d8c5d257b7b1a7869a
yonatankra <yonatan.kra@vonage.com>
2020-10-10 07:20:15 +0300

    greet test fails

--- packages/greeter/src/lib/greeter.spec.ts
+++ packages/greeter/src/lib/greeter.spec.ts
@@ -4,4 +4,14 @@ describe('greeter', () => {
   it(`should exist`, () => {
     expect(Greeter).toBeDefined();
   });
+
+  describe(`greet`, function() {
+    it(`should prepend hello to name input`, function() {
+      const greeter = new Greeter();
+      const name = 'some name';
+      const expectedResult = 'Hello ' + name;
+      expect(greeter.greet(name)).toEqual(expectedResult)
+    });
+  });
+
 });
