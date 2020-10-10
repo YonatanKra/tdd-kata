d7b362285450b69f8915cc6f8e5d024baf6228ca
yonatankra <yonatan.kra@vonage.com>
2020-10-10 10:33:14 +0300

    Greeter test passes
    But all the rest fail :(

--- packages/greeter/src/lib/greeter.ts
+++ packages/greeter/src/lib/greeter.ts
@@ -1,10 +1,17 @@
 function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
 export class Greeter {
+  constructor(private date = new Date()) {
+  }
 
   greet(name: string) {
     if (typeof name !== 'string') throw new Error('You must provide a string to Greeter.greet');
-    return 'Hello ' + capitalize(name.trim());
+    let prefix = 'Hello ';
+    if ((this.date.getHours() >= 6 && this.date.getHours() < 12) || (this.date.getHours() === 12 && this.date.getMinutes() === 0)) {
+      prefix = 'Good morning ';
+    }
+    return prefix + capitalize(name.trim());
   }
 }
