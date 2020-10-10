e3e70124a8dfdab8703b832b8e325792da0fa613
yonatankra <yonatan.kra@vonage.com>
2020-10-10 07:33:09 +0300

    Test for bullet 3

--- packages/greeter/src/lib/greeter.spec.ts
+++ packages/greeter/src/lib/greeter.spec.ts
@@ -26,6 +26,14 @@ describe('greeter', () => {
       const name = {a: 1};
       expect(() => greeter.greet(name as unknown as string)).toThrow('You must provide a string to Greeter.greet');
     });
+
+    it(`should capitalize the first letter of the name`, function() {
+      const greeter = new Greeter();
+      const name = 'jon doe';
+      const capitalizedName = 'Jon doe';
+      const expectedResult = 'Hello ' + capitalizedName;
+      expect(greeter.greet(name)).toEqual(expectedResult);
+    });
   });
 
 });
