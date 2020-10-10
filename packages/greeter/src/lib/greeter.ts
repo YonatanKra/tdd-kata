22a2a7fa52123a0e6ec76a9e3d5a3333d7a98e7d
yonatankra <yonatan.kra@vonage.com>
2020-10-10 07:36:28 +0300

    Capitalize test passes
    But some old tests failed - cause the API changed!!!

--- packages/greeter/src/lib/greeter.ts
+++ packages/greeter/src/lib/greeter.ts
@@ -1,7 +1,10 @@
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 export class Greeter {
 
   greet(name: string) {
     if (typeof name !== 'string') throw new Error('You must provide a string to Greeter.greet');
-    return 'Hello ' + name.trim();
+    return 'Hello ' + capitalize(name.trim());
   }
 }
