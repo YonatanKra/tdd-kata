441ce35435ee63734b591c05319c71d5cd0f5ab7
yonatankra <yonatan.kra@vonage.com>
2020-10-15 09:26:04 +0300

    Major refactor - tests still pass!

--- packages/simple-agent-path-optimization/src/lib/simple-agent-path-optimization.spec.ts
+++ packages/simple-agent-path-optimization/src/lib/simple-agent-path-optimization.spec.ts
@@ -1,22 +1,8 @@
-import { countSimilarElements, optimizeAgent } from '@tdd-kata/simple-agent-path-optimization';
+import { optimizeAgent } from '@tdd-kata/simple-agent-path-optimization';
 
 
 
 describe(`simple agent path optimization`, function() {
-
-  describe(`countSimilarElements`, function() {
-    it(`should return null if given the last element`, function() {
-      const input = [2,1,1,1,2];
-      const startingIndex = input.length - 1;
-      expect(countSimilarElements(input, startingIndex)).toEqual(null);
-    });
-
-    it(`should return the index of the next different element`, function() {
-      const input = [2,1,1,1,2];
-      const startingIndex = 1;
-      expect(countSimilarElements(input, startingIndex)).toEqual(4);
-    });
-  });
 
   describe(`optimizeAgent`, function() {
     it(`should return empty array if given empty array`, function() {
--- packages/simple-agent-path-optimization/src/lib/simple-agent-path-optimization.ts
+++ packages/simple-agent-path-optimization/src/lib/simple-agent-path-optimization.ts
@@ -4,13 +4,6 @@ const OPPOSITES = {
   LEFT: "RIGHT",
   RIGHT: "LEFT"
 }
-export function countSimilarElements(list: any[], startingIndex: number): number {
-  const currentElement = list[startingIndex];
-  for (let i = startingIndex + 1; i < list.length; i++) {
-    if (list[i] !== currentElement) return i;
-  }
-  return null;
-}
 
 function removeOpposites(result: any[], startingPosition: number): boolean {
   if (result[startingPosition - 1] === OPPOSITES[result[startingPosition]]) {
@@ -20,14 +13,12 @@ function removeOpposites(result: any[], startingPosition: number): boolean {
   return false;
 }
 
-export function optimizeAgent(input: any[]) {
-  const result = [...input];
-  let startingPosition = 0;
-  while (startingPosition !== null) {
-    startingPosition = countSimilarElements(result, startingPosition);
-    if (startingPosition !== null) {
-      if (removeOpposites(result, startingPosition)) startingPosition-=2;
-    }
-  }
-  return result;
+export function optimizeAgent(plan: any[]) {
+  return plan.reduce(function(dirs, dir){
+    if (dirs[dirs.length - 1] === OPPOSITES[dir])
+      dirs.pop();
+    else
+      dirs.push(dir);
+    return dirs;
+  }, []);
 }
