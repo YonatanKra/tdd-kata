669fbdc9e65dd0cf0929af6bb206013292438a38
yonatankra <yonatan.kra@vonage.com>
2020-10-19 09:15:36 +0300

    Refactor

--- packages/calc-stats/src/lib/calc-stats.ts
+++ packages/calc-stats/src/lib/calc-stats.ts
@@ -5,9 +5,7 @@ function throwError(errorMessage) {
 export function minimumValue(sequence: number[]) {
   if (!Array.isArray(sequence)) throwError('sequence must be an array');
   if (sequence.length === 0) throwError('Array cannot be empty.');
-  let minValueInSequence = Infinity;
-  for (let i = 0; i < sequence.length; i++) {
-    if (sequence[i] < minValueInSequence) minValueInSequence = sequence[i];
-  }
-  return minValueInSequence;
+
+  return sequence.reduce((currMin, nextVal) => currMin < nextVal ? currMin : nextVal, Infinity);
+  
 }
