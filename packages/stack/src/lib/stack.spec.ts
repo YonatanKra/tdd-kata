9eea794ba4161ec49e19d1733ca81a0c840c3635
yonatankra <yonatan.kra@vonage.com>
2020-10-19 13:35:16 +0300

    Is empty passes

--- packages/stack/src/lib/stack.spec.ts
+++ packages/stack/src/lib/stack.spec.ts
@@ -4,4 +4,9 @@ describe(`stack`, function() {
   it(`should exist`, function() {
     expect(new Stack()).toBeDefined();
   });
+
+  it(`should be empty`, function() {
+    const myStack = new Stack();
+    expect(myStack.isEmpty()).toEqual(true);
+  });
 });
