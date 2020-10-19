6de11e4c189b0de1b277fdccbc15ffa6e51de799
yonatankra <yonatan.kra@vonage.com>
2020-10-19 09:11:16 +0300

    Small refactor

--- packages/calc-stats/src/lib/calc-stats.ts
+++ packages/calc-stats/src/lib/calc-stats.ts
@@ -1,4 +1,8 @@
+function throwError(errorMessage) {
+  throw new Error(errorMessage);
+}
+
 export function minimumValue(sequence: number[]) {
-  if (!Array.isArray(sequence)) throw new Error('sequence must be an array');
-  throw new Error('Array cannot be empty.');
+  if (!Array.isArray(sequence)) throwError('sequence must be an array');
+  throwError('Array cannot be empty.');
 }
