25ecf6488d1ce0421f46f00f194a2a8396eaa3ce
yonatankra <yonatan.kra@vonage.com>
2020-10-19 09:14:23 +0300

    Test passes

--- packages/calc-stats/src/lib/calc-stats.ts
+++ packages/calc-stats/src/lib/calc-stats.ts
@@ -4,5 +4,10 @@ function throwError(errorMessage) {
 
 export function minimumValue(sequence: number[]) {
   if (!Array.isArray(sequence)) throwError('sequence must be an array');
-  throwError('Array cannot be empty.');
+  if (sequence.length === 0) throwError('Array cannot be empty.');
+  let minValueInSequence = Infinity;
+  for (let i = 0; i < sequence.length; i++) {
+    if (sequence[i] < minValueInSequence) minValueInSequence = sequence[i];
+  }
+  return minValueInSequence;
 }
