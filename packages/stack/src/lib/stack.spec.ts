907c1386aeafc299f86ea3b22745adf5839139a7
yonatankra <yonatan.kra@vonage.com>
2020-10-19 14:02:21 +0300

    Refactor the tests

--- packages/stack/src/lib/stack.spec.ts
+++ packages/stack/src/lib/stack.spec.ts
@@ -1,30 +1,32 @@
 import { Stack } from '@tdd-kata/stack';
 
 describe(`stack`, function() {
+  let myStack: Stack;
+
+  beforeEach(function() {
+    myStack = new Stack();
+  });
+
   it(`should exist`, function() {
     expect(new Stack()).toBeDefined();
   });
 
   it(`should be empty`, function() {
-    const myStack = new Stack();
     expect(myStack.isEmpty()).toEqual(true);
   });
 
   it(`should not be empty after push`, function() {
-    const myStack = new Stack();
     myStack.push(1);
     expect(myStack.isEmpty()).toEqual(false);
   });
 
   it(`should be empty after one push and one pop`, function() {
-    const myStack = new Stack();
     myStack.push(1);
     myStack.pop();
     expect(myStack.isEmpty()).toEqual(true);
   });
 
   it(`should not be empty after two pushes and one pop`, function() {
-    const myStack = new Stack();
     myStack.push(1);
     myStack.push(2);
     myStack.pop();
